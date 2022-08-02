96177955671b653efcb83f4b2a81152c4861cbee
Pasindu Sahan <pasindusahan001@gmail.com>
2022-08-02 21:22:40 +0530

    Add markers for pickup location & dropoff location

--- pages/Components/Map.js
+++ pages/Components/Map.js
@@ -3,25 +3,40 @@ import { useEffect } from 'react'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw';
 
-const Map = () => {
-    useEffect(()=> {
+const Map = (props) => {
+  //console.log(props)
+  useEffect(()=> {
     const map = new mapboxgl.Map({
-    container: 'map', 
-    style: 'mapbox://styles/mapbox/streets-v11', 
-    center: [79.890000, 6.911970], 
-    zoom: 12, 
-    // projection: 'globe' 
+      container: 'map', 
+      style: 'mapbox://styles/mapbox/streets-v11', 
+      center: [79.890000, 6.911970], 
+      zoom: 12, 
     });
-    add_to_map(map)
-    
-  })
 
-  const add_to_map = (map) => {
+    if (props.pickup_cordinates){
+      add_to_map(map, props.pickup_cordinates)
+    }
+
+    if (props.dropoff_cordinates){
+      add_to_map(map, props.dropoff_cordinates)
+    }
+
+    if (props.pickup_cordinates && props.dropoff_cordinates) {
+      map.fitBounds([
+        props.pickup_cordinates,
+        props.dropoff_cordinates
+      ], {
+        padding: 60
+      })
+    }
+    
+  },[props.pickup_cordinates, props.dropoff_cordinates])
+
+  const add_to_map = (map, cordinates) => {
     const marker1 = new mapboxgl.Marker()
-    .setLngLat([79.89, 7.0])
+    .setLngLat(cordinates)
     .addTo(map)
   }
-
 
   return <div className='map' id='map'>Map</div>
    
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,55 +1,60 @@
 import React from 'react'
 import Map from './Components/Map'
 import styles from '../styles/Confirm.module.css'
-import { useEffect } from 'react'
-
-
+import { useEffect, useState } from 'react'
 
 const Confirm = () => {
 
-    const get_pickup_cordinates = () => {
-        const pickup_location = "Colombo";
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup_location}.json?` + 
-            new URLSearchParams({
-                access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
-                limit: 1
-            })
-        )
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.features[0].center)
-        })
-    }
-
-    const get_dropoff_cordinates = () => {
-        const dropoff_location = "Maharagama";
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff_location}.json?` + 
-            new URLSearchParams({
-                access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
-                limit: 1
-            })
-        )
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.features[0].center)
-        })
-    }
+  const [pickup_cordinates, set_pickup_cordinates] = useState()
+  const [dropoff_cordinates, set_dropoff_cordinates] = useState()
 
 
-    useEffect(()=> {
-        get_pickup_cordinates();
-        get_dropoff_cordinates();
-    },[])
+  const get_pickup_cordinates = () => {
+    const pickup_location = "Kottawa";
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup_location}.json?` + 
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
+        limit: 1
+      })
+    )
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data.features[0].center)
+    set_pickup_cordinates(data.features[0].center)
+    })
+  }
 
-    return (
-        <div className={styles.body}>
-            <div className={styles.container}>
-                <Map/>
-                {/* <div className={styles.ride_container}>
-                    Ride selector
-                </div> */}
-            </div>
+  const get_dropoff_cordinates = () => {
+    const dropoff_location = "Padukka";
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff_location}.json?` + 
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
+        limit: 1
+      })
+    )
+    .then(response => response.json())
+    .then(data => {
+    //console.log(data.features[0].center)
+    set_dropoff_cordinates(data.features[0].center)
+    })
+  }
+
+  useEffect(()=> {
+    get_pickup_cordinates();
+    get_dropoff_cordinates();
+  },[])
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <Map pickup_cordinates={pickup_cordinates} dropoff_cordinates = {dropoff_cordinates}/>
+        <div className={styles.ride_container}>
+          Ride selector
+          {pickup_cordinates}
+          {dropoff_cordinates}
         </div>
+      </div>
+    </div>
   )
 }
 
