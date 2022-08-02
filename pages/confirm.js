4deb1f6cc6acf27fe7fde24cde71b1e4d12f0ffd
Pasindu Sahan <pasindusahan001@gmail.com>
2022-08-02 22:40:22 +0530

    Add input for add pickup & dropoff locations

--- pages/confirm.js
+++ pages/confirm.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Map from './Components/Map'
 import styles from '../styles/Confirm.module.css'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const Confirm = () => {
+  const router = useRouter()
+  const {pickup, dropoff} = router.query
+
+  console.log("Pickup: ", pickup)
+  console.log("dropoff: ", dropoff)
 
   const [pickup_cordinates, set_pickup_cordinates] = useState()
   const [dropoff_cordinates, set_dropoff_cordinates] = useState()
 
 
-  const get_pickup_cordinates = () => {
-    const pickup_location = "Kottawa";
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup_location}.json?` + 
+  const get_pickup_cordinates = (pickup) => {
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` + 
       new URLSearchParams({
         access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
         limit: 1
@@ -24,9 +29,8 @@ const Confirm = () => {
     })
   }
 
-  const get_dropoff_cordinates = () => {
-    const dropoff_location = "Padukka";
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff_location}.json?` + 
+  const get_dropoff_cordinates = (dropoff) => {
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` + 
       new URLSearchParams({
         access_token: "pk.eyJ1IjoicGFzaW5kdS1zYWhhbiIsImEiOiJjbDZhcWNjZnkxcTZ4M2JuM2EyenljZzNtIn0.1kQi7s34sfJys5XlkCFahw",
         limit: 1
@@ -40,9 +44,9 @@ const Confirm = () => {
   }
 
   useEffect(()=> {
-    get_pickup_cordinates();
-    get_dropoff_cordinates();
-  },[])
+    get_pickup_cordinates(pickup);
+    get_dropoff_cordinates(dropoff);
+  },[pickup, dropoff])
 
   return (
     <div className={styles.body}>
--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import styles from '../styles/Search.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Search = () => {
+    const [pickup, set_pickup] = useState("")
+    const [dropoff, set_dropoff] = useState("")
+
+    console.log(pickup)
+    console.log(dropoff)
+
   return (
     <div className={styles.body}>
         <div className='row'>
@@ -14,9 +21,17 @@ const Search = () => {
         <div className='row mt-4'>
             <div className={styles.container}>
                 <h2 className={styles.title}>Request a ride</h2>
-                <input className={styles.input} placeholder="Pickup location"/>
-                <input className={styles.input} placeholder="Destination location"/>
-                <button type="button" className="btn btn-outline-primary">Request Ride</button>
+                <input className={styles.input} placeholder="Pickup location" value={pickup} onChange={(e) => set_pickup(e.target.value)}/>
+                <input className={styles.input} placeholder="Destination location" value={dropoff} onChange={(e) => set_dropoff(e.target.value)}/>
+                <Link href={{
+                    pathname: "/confirm",
+                    query: {
+                        pickup: pickup,
+                        dropoff: dropoff
+                    }
+                }}>
+                    <button type="button" className="btn btn-outline-primary">Request Ride</button>
+                </Link>
             </div>
             
         </div>
