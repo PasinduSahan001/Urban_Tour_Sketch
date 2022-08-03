7a99faef08a8eefad1279218477dc987cc9700b3
Kesara Lakpriya <98794331+42Kesara@users.noreply.github.com>
2022-08-03 16:35:13 +0530

    fuel available page

--- pages/index.js
+++ pages/index.js
@@ -13,57 +13,51 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Urban Tour
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <a className={styles.card}>
+            <h2>Morotuwa IOC Filling station</h2>
+            <p className={styles.loopp}>Available Fuel Types</p>
+            <div>
+            <ul>
+              <li><button>P92</button></li>
+              <li><button>P95</button></li>
+              <li><button>D</button></li>
+              <li><button>SD</button></li>
+            </ul>   
+            </div>
+              <hr/>
+            
+            <p className={styles.loopp}>Fuel volume for the day <br></br>2022/08/03</p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <div>
+              <label>Petrol-92 :</label>
+              <a className={styles.barr}> 250L</a> <br></br>
+              <label>Petrol-95 :</label>
+              <a  className={styles.barr}> 250L</a><br></br>
+              <label>Diesel :</label>
+              <a  className={styles.barr}> 250L</a><br></br>
+              <label>Super-Diesel :</label>
+              <a  className={styles.barr}> 250L</a><br></br>
+            </div>
+            <hr/>
+          <button className={styles.hjk}>Book Time Slot!</button>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </a> 
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a>
+          Copyright ©️ 2022. All rights reserved.
+                        Designed & developed by GROUP-16
         </a>
       </footer>
+
     </div>
   )
 }
